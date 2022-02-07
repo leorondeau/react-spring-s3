9ae78076c0dd6cd16316eb94a06923faec3d5bfc
Leo Rondeau <christopherleorondeau@gmail.com>
2022-02-07 15:48:49 -0600

    tutorial uses hard coded credentials, attempting to set up secrets manager but still working on it

--- src/main/java/com/leo2020/awsimageupload/config/AmazonConfig.java
+++ src/main/java/com/leo2020/awsimageupload/config/AmazonConfig.java
@@ -1,12 +1,15 @@
 package com.leo2020.awsimageupload.config;
 
-import com.amazonaws.auth.AWSCredentials;
-import com.amazonaws.auth.AWSStaticCredentialsProvider;
-import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.auth.*;
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.AmazonS3ClientBuilder;
+import com.amazonaws.services.secretsmanager.AWSSecretsManager;
+import com.amazonaws.services.secretsmanager.AWSSecretsManagerClientBuilder;
+import com.amazonaws.services.secretsmanager.model.*;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.util.Base64;
 
 //Spring instantiates Config and anything in the class
 @Configuration
@@ -14,14 +17,16 @@ public class AmazonConfig {
 
     //give us the s3 client
     @Bean
-    public AmazonS3 s3() { //comes from dependency added in pom.xml
+    public AmazonS3 s3() {
         AWSCredentials awsCredentials = new BasicAWSCredentials(
-               accessKey: "AKIAY3JGGVL5HAPBYOE5",
-               secretKey: "4W9dJVSw0Kv7ThD3qcEGmWtKc1bUrygsEFMVcXZ"
+           accessKey: "",
+           secretKey: ""
         );
-        return AmazonS3ClientBuilder
-                .standard()
-                .withCredentials(new AWSStaticCredentialsProvider(awsCredentials))
-                .build();
-    }
+
+
+    return AmazonS3ClientBuilder
+            .standard()
+            .withCredentials(new AWSStaticCredentialsProvider(awsCredentials))
+            .build();
+}
 }
